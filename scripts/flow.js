941dab49f1cb4f14ed84aa3a23b8b198ba16b02a
syaiful-aa <syaiful.salam@agriaku.com>
2023-03-01 09:37:07 +0700

    update

--- scripts/flow.js
+++ scripts/flow.js
@@ -4,5 +4,4 @@ function getData() {
 }
 
 // getData()
-console.log('koplak')
 console.log('kocak')
--- scripts/flow.sh
+++ scripts/flow.sh
@@ -2,5 +2,7 @@
 
 message="oit"
 echo "$OUTPUT1 $OUTPUT2 from inside"
+echo "args $1 $2 $3"
 echo "test=oey oey" >> "$GITHUB_OUTPUT" 
 echo "message=$message" >> "$GITHUB_OUTPUT" 
+echo $GITHUB_OUTPUT
