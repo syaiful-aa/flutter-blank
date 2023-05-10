c3452a66e39421c61831215a08363c76386b7f71
syaiful-aa <syaiful.salam@agriaku.com>
2023-05-10 14:49:30 +0700

    x

--- .github/workflows/split.sh
+++ .github/workflows/split.sh
@@ -15,3 +15,23 @@ for option in "${optionsSplitted[@]}"; do
   echo $option
 done
 echo "${optionsSplitted[@]}"
+
+branches=(master dev prod)
+JSON="["
+for branch in ${branches[@]}; do
+    echo $branch
+    JSONline="\"$branch\","
+    # we don't need to iterate on the same branch over and over, so
+    # onnly include it when it wasn't included
+    if [[ "$JSON" != *"$JSONline"* ]]; then
+        JSON="$JSON$JSONline"
+    fi
+done
+
+# Remove last "," and add the closing bracket
+if [[ $JSON == *, ]]; then
+    JSON="${JSON%?}"
+fi
+JSON="$JSON]"
+
+echo $JSON
