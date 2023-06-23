8ada03d5f178e6da32240ece3d3e9b4f00393bc8
syaiful-aa <syaiful.salam@agriaku.com>
2023-06-23 13:51:03 +0700

    fix code smell

--- lib/main.dart
+++ lib/main.dart
@@ -49,7 +49,6 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-  var _dump;
 
   void _incrementCounter() {
     setState(() {
